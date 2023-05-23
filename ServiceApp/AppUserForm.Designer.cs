4dfad875e931d7e6b4705d13c2902abd68b5198d
LogExE <awesome149712@gmail.com>
2023-05-23 17:10:05 +0400

    adm

---
+++ ServiceApp/AppUserForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ServiceApp
+{
+    partial class AppUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pwdText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginText = new System.Windows.Forms.TextBox();
+            this.proceedButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.admCheck = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 164F));
+            this.tableLayoutPanel1.Controls.Add(this.pwdText, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.loginText, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.proceedButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.admCheck, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(30, 50, 30, 30);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.00145F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.00146F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.99896F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.99813F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(369, 333);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // pwdText
+            // 
+            this.pwdText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pwdText.Location = new System.Drawing.Point(178, 116);
+            this.pwdText.Name = "pwdText";
+            this.pwdText.Size = new System.Drawing.Size(158, 23);
+            this.pwdText.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(33, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 63);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(33, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 63);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Пароль";
+            // 
+            // loginText
+            // 
+            this.loginText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginText.Location = new System.Drawing.Point(178, 53);
+            this.loginText.Name = "loginText";
+            this.loginText.Size = new System.Drawing.Size(158, 23);
+            this.loginText.TabIndex = 21;
+            // 
+            // proceedButton
+            // 
+            this.proceedButton.Location = new System.Drawing.Point(178, 242);
+            this.proceedButton.Name = "proceedButton";
+            this.proceedButton.Size = new System.Drawing.Size(75, 23);
+            this.proceedButton.TabIndex = 18;
+            this.proceedButton.Text = "Далее";
+            this.proceedButton.UseVisualStyleBackColor = true;
+            this.proceedButton.Click += new System.EventHandler(this.proceedButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(33, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 63);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "label3";
+            // 
+            // admCheck
+            // 
+            this.admCheck.AutoSize = true;
+            this.admCheck.Location = new System.Drawing.Point(178, 179);
+            this.admCheck.Name = "admCheck";
+            this.admCheck.Size = new System.Drawing.Size(118, 19);
+            this.admCheck.TabIndex = 24;
+            this.admCheck.Text = "Администратор?";
+            this.admCheck.UseVisualStyleBackColor = true;
+            // 
+            // AppUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 333);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "AppUserForm";
+            this.Text = "AppUserForm";
+            this.Load += new System.EventHandler(this.AppUserForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button proceedButton;
+        private TextBox pwdText;
+        private Label label1;
+        private Label label2;
+        private TextBox loginText;
+        private Label label3;
+        private CheckBox admCheck;
+    }
+}
---
+++ ServiceApp/AppUserForm.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Core;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceApp
+{
+    public partial class AppUserForm : Form
+    {
+        Dictionary<string, object> fields;
+        public AppUserForm(Dictionary<string, object> fields)
+        {
+            InitializeComponent();
+            this.fields = fields;
+        }
+
+        private void AppUserForm_Load(object sender, EventArgs e)
+        {
+            if (fields != null)
+            {
+                loginText.Text = (string)fields["Login"];
+                pwdText.Text = (string)fields["Passwd"];
+                admCheck.Checked = (bool)fields["IsAdmin"];
+            }
+        }
+
+        private void proceedButton_Click(object sender, EventArgs e)
+        {
+            using var con = new SqlConnection(ConfigurationManager.ConnectionStrings["MSSQL"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+
+            string adm = admCheck.Checked ? "1" : "0";
+
+            if (fields != null)
+                cmd.CommandText = $"UPDATE AppUser SET Login = N'{loginText.Text}', Passwd = N'{pwdText.Text}', IsAdmin = {adm} WHERE ID = {fields["ID"]}";
+            else
+                cmd.CommandText = $"INSERT INTO AppUser (Login, Passwd, IsAdmin) VALUES (N'{loginText.Text}', N'{pwdText.Text}', {adm})";
+            Debug.WriteLine(cmd.CommandText);
+            cmd.ExecuteNonQuery();
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
